a196299c747f7b3d39f0bb7f151b03fe197e4e3f
Yonle <yonle@lecturify.net>
2024-09-22 15:18:30 +0700

    use http.Redirect instead
    Signed-off-by: Yonle <yonle@lecturify.net>

--- request_handler.go
+++ request_handler.go
@@ -29,7 +29,7 @@ func request_handler(w http.ResponseWriter, r *http.Request) {
 
 	resp, err := proxy(ctx, r, origin_url)
 	if err != nil {
-		redirect(w, origin_url)
+		http.Redirect(w, r, origin_url, http.StatusFound)
 		return
 	}
 
@@ -37,7 +37,7 @@ func request_handler(w http.ResponseWriter, r *http.Request) {
 
 	if err := process_image(w, resp, quality, grayscale); err != nil {
 		log.Printf("Failed to process %s: %s", origin_url, err)
-		redirect(w, origin_url)
+		http.Redirect(w, r, origin_url, http.StatusFound)
 		return
 	}
 }
