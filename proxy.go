c390c7e069124bd7ad89355afa7631f4aa189eda
Yonle <yonle@proton.me>
2026-02-03 01:58:42 +0700

    deal with headers
    Signed-off-by: Yonle <yonle@proton.me>

--- proxy.go
+++ proxy.go
@@ -8,23 +8,21 @@ import (
 
 var hc = http.Client{
 	Timeout: 10 * time.Second,
+	Transport: &http.Transport{
+		DisableCompression: true,
+	},
 }
 
 func proxy(ctx context.Context, r *http.Request, origin_url string) (resp *http.Response, err error) {
-	clientHeader := r.Header.Clone()
-	clientHeader.Del("Accept-Encoding")
-	clientHeader.Set("User-Agent", "go-bwhero")
-	clientHeader.Set("Via", "2.0 go-bwhero")
-
 	req, err := http.NewRequestWithContext(ctx, r.Method, origin_url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header = clientHeader
-	for _, cookie := range r.Cookies() {
-		req.AddCookie(cookie)
-	}
+	copyClientHeaders(req.Header, r.Header)
+	req.Header.Set("Accept", "image/*")
+	req.Header.Set("User-Agent", "go-bwhero")
+	req.Header.Set("Via", "2.0 go-bwhero")
 
 	return hc.Do(req)
 }
