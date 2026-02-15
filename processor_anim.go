96a7176d2971536e8dd3a8469954ede5ccb0ecfe
Yonle <yonle@proton.me>
2026-02-15 16:40:32 +0700

    hello processor_anim
    Signed-off-by: Yonle <yonle@proton.me>

---
+++ processor_anim.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os/exec"
+	"strconv"
+)
+
+func process_anim(ctx context.Context, w http.ResponseWriter, resp *http.Response, quality, grayscale int) error {
+	filters := "null"
+
+	if grayscale == 1 {
+		filters = "hue=s=0"
+	}
+
+	cmd := exec.CommandContext(ctx, "ffmpeg",
+		"-loglevel", "error",
+		"-i", "pipe:0", // Input from stdin
+		"-vf", filters, // Video filters (Greyscale)
+		"-c:v", "libwebp_anim", // WebP encoder
+		"-loop", "0", // Infinite loop
+		"-q:v", strconv.Itoa(quality),
+		"-compression_level", "4",
+		"-f", "webp", // Output format
+		"pipe:1", // Output to stdout
+	)
+
+	cmd.Stdin = resp.Body
+	cmd.Stdout = w
+
+	h := w.Header()
+	h.Set("Access-Control-Allow-Origin", "*")
+	h.Set("Cross-Origin-Resource-Policy", "cross-origin")
+	h.Set("Cross-Origin-Embedder-Policy", "unsafe-none")
+	h.Set("Cache-Control", "public, max-age=604800, stale-while-revalidate=86400") // cache for a week while asking for revalidation after a day
+	h.Set("Content-Encoding", "identity")
+	h.Set("Content-Type", "image/webp")
+	h.Set("Transfer-Encoding", "chunked")
+	h.Set("X-Original-Size", strconv.FormatInt(resp.ContentLength, 10))
+
+	w.WriteHeader(200)
+
+	return cmd.Run()
+}
